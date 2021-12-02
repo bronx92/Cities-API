190c6e60c5d424b18245d0028f3d108c3b4f7073
bronx92 <especialcontas@outlook.com>
2021-12-02 00:12:31 -0300

    Adiciona metodo de busca por Id

--- src/main/java/com/github/bronx92/citiesapi/controller/CountryResource.java
+++ src/main/java/com/github/bronx92/citiesapi/controller/CountryResource.java
@@ -4,9 +4,13 @@ import com.github.bronx92.citiesapi.model.Country;
 import com.github.bronx92.citiesapi.repository.CountryRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -22,4 +26,26 @@ public class CountryResource {
     public Page<Country> countries(Pageable page) {
         return repository.findAll(page);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getOne(@PathVariable Long id) {
+        Optional<Country> optional = repository.findById(id);
+        if (optional.isPresent()) {
+            return ResponseEntity.ok().body(optional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    // o método poderia ser feito das seguintes formas:
+    /*
+        public Optional<Country> getOne(@PathVariable Long id) {
+            return repository.findById(id);
+        }
+        Ou
+        public Country getOne(@PathVariable Long id) {
+            Optional<Country> optional = repository.findById(id);
+            return optional.get();
+        }
+        Mas fazendo dessas maneiras não há um tratamento de exceção para o caso de busca por um id não existente, que nesse caso é feito pelo ResponseEntity
+    */
 }
