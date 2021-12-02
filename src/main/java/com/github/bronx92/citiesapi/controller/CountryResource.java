b3b16868f61004387111f0251b15f7020f54e50b
bronx92 <especialcontas@outlook.com>
2021-12-01 23:31:44 -0300

    Adiciona paginacao com Spring

--- src/main/java/com/github/bronx92/citiesapi/controller/CountryResource.java
+++ src/main/java/com/github/bronx92/citiesapi/controller/CountryResource.java
@@ -2,11 +2,11 @@ package com.github.bronx92.citiesapi.controller;
 
 import com.github.bronx92.citiesapi.model.Country;
 import com.github.bronx92.citiesapi.repository.CountryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/countries")
@@ -19,7 +19,7 @@ public class CountryResource {
     }
 
     @GetMapping
-    public List<Country> countries() {
-        return repository.findAll();
+    public Page<Country> countries(Pageable page) {
+        return repository.findAll(page);
     }
 }
